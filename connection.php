0a86faa64928e106d9b5d9d1ace926563f788739
Atharva-1144 <Atharvadiwan666@gmail.com>
2024-12-26 10:21:07 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php 
+// $host="localhost";
+// $dbname='tryok';
+// $username='root';
+// $password='';
+
+// $pdo=new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+ $con = mysqli_connect('localhost', 'root', '', 'tryok');
+
+?>
